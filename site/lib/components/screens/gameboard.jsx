930bb135ad3406931671cf3d29437b9f6621e7f5
0x0259 <starkey.joshua@gmail.com>
2015-02-19 22:51:43 -0800

    Improve gameboard layout.

--- site/lib/components/screens/gameboard.jsx
+++ site/lib/components/screens/gameboard.jsx
@@ -5,96 +5,34 @@ var App = require('../app.jsx');
 var Gameboard = React.createClass({
   getInitialState: function getInitialState() {
     return {
-      hoverTile: [null, null],
-      selectedTile: [null, null]
+      shouldShowGrid: false
     }
   },
-  calculateBoardTile: function getBoardLocation(xOffset, yOffset, xCoord, yCoord) {
-    var pointA = [0,0];
-    var pointB = [-100,50];
-    var currentX = -1;
-    var currentY = -1;
-
-    while (true) {
-      if (Math.sign((pointB[0]-pointA[0])*(yCoord-pointA[1]) - (pointB[1]-pointA[1])*(xCoord-pointA[0])) === -1) {
-        pointA[0] = pointA[0] + 100;
-        pointA[1] = pointA[1] + 50;
-        pointB[0] = pointB[0] + 100;
-        pointB[1] = pointB[1] + 50;
-        currentY++;
-      } else {
-        break;
-      }
-    }
-
-    pointA = [0,0];
-    pointB = [100,50];
-
-    while (true) {
-      if (Math.sign((pointB[0]-pointA[0])*(yCoord-pointA[1]) - (pointB[1]-pointA[1])*(xCoord-pointA[0])) === 1) {
-        pointA[0] = pointA[0] - 100;
-        pointA[1] = pointA[1] + 50;
-        pointB[0] = pointB[0] - 100;
-        pointB[1] = pointB[1] + 50;
-        currentX++;
-      } else {
-        break;
-      }
-    }
-
-    return [currentX, currentY];
-  },
-  setHoverTile: function setHoverTile(event) {
-    if (event.target.className.indexOf('gameboard__tile') !== -1) {
-      var xOffset = document.getElementById('gameboard').offsetWidth - (document.getElementById('gameboard').offsetWidth - 100) / 2 - 50;
-      var yOffset = document.getElementById('gameboard').offsetHeight / 10 - 10;
-      var xCoord = (event.clientX - xOffset);
-      var yCoord = (event.clientY - yOffset);
-
-      var hoverTile = this.calculateBoardTile(xOffset, yOffset, xCoord, yCoord);
-
-      this.setState({ hoverTile: hoverTile });
-    }
-  },
-  setSelectedTile: function setSelectedTile(event) {
-    if (event.target.className.indexOf('gameboard__tile') !== -1) {
-      var xOffset = document.getElementById('gameboard').offsetWidth - (document.getElementById('gameboard').offsetWidth - 100) / 2 - 50;
-      var yOffset = document.getElementById('gameboard').offsetHeight / 10 - 10;
-      var xCoord = (event.clientX - xOffset);
-      var yCoord = (event.clientY - yOffset);
-
-      var selectedTile = this.calculateBoardTile(xOffset, yOffset, xCoord, yCoord);
-
-      this.setState({ selectedTile: selectedTile });
-    }
-  },
-  clearHoverTile: function clearHoverTile() {
-    this.setState({ hoverTile: [null, null] });
-  },
-  clearSelectedTile: function clearSelectedTile() {
-    this.setState({ selectedTile: [null, null] });
+  toggleGrid: function toggleGrid() {
+    this.setState({ shouldShowGrid: !this.state.shouldShowGrid });
   },
   renderBoard: function renderBoard(x, y) {
     var board = [];
 
     for (var xIndex = 0; xIndex < x; xIndex++) {
       for (var yIndex = 0; yIndex < y; yIndex++) {
-        var isHovered = this.state.hoverTile[0] === xIndex && this.state.hoverTile[1] === yIndex;
-        var isSelected = this.state.selectedTile[0] === xIndex && this.state.selectedTile[1] === yIndex;
-        board.push(<Tile key={xIndex + ',' + yIndex} x={xIndex} y={yIndex} scalar={this.props.tileScalar || 100} isHovered={isHovered} isSelected={isSelected} />);
+        board.push(<Tile key={xIndex + ',' + yIndex} x={xIndex} y={yIndex} scalar={this.props.tileScalar || 100} style='' />);
       }
     }
 
     return board;
   },
   render: function () {
-    var self = this;
+    var gameboardClassName = 'gameboard' + ((this.state.shouldShowGrid) ? ' gameboard--grid' : '');
 
     return (
       <App>
         <div className='grid-background' />
-        <div className='gameboard' id='gameboard' onMouseMove={self.setHoverTile} onMouseOut={self.clearHoverTile} onClick={self.setSelectedTile}>
-          {self.renderBoard(8, 8)}
+        <div className={gameboardClassName} id='gameboard'>
+          {this.renderBoard(7, 7)}
+        </div>
+        <div className='gameboard-options'>
+          <button onClick={this.toggleGrid}>Toggle Grid</button>
         </div>
       </App>
     );
--- site/lib/components/tiles/tile.jsx
+++ site/lib/components/tiles/tile.jsx
@@ -4,17 +4,20 @@ var Tile = React.createClass({
   render: function () {
     var x = this.props.x;
     var y = this.props.y;
-    var className = 'gameboard__tile gameboard__tile--' + (this.props.color || 'default') + (this.props.isHovered ? ' gameboard__tile--hover' : '') + (this.props.isSelected ? ' gameboard__tile--selected' : '');
+    var className = 'gameboard__tile gameboard__tile--' + (this.props.style || 'default') + (this.props.isSelected ? ' gameboard__tile--selected' : '');
 
     var tileStyle = {
-      top: (x + y) * (this.props.scalar / 2),
-      left: 'calc(' + ((y - x) * this.props.scalar - 150) + 'px + 50%)',
+      top: (x + y) * 58,
+      left: 'calc(' + ((y - x) * this.props.scalar) + 'px + 50% - 100px)',
       zIndex: x + y
     }
 
+    var assets = [<div className='gameboard__tile__shadow' />];
+
     return (
       <div className={className} style={tileStyle} data-x-coordinate={x} data-y-coordinate={y}>
-        <div className='gameboard__tile__shadow' />
+        <div className='gameboard__tile__contents' />
+        <div className='gameboard__tile__edge' />
       </div>
     );
   }
